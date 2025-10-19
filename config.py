ae4d95bc1ec080df60c62e81f0c66ba6a0d0d2f9
JuanSeromo-UniAndes <js.rodriguezm12345@uniandes.edu.co>
2025-10-19 10:12:03 -0500

    added eb configs

--- config.py
+++ config.py
@@ -2,6 +2,17 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:mysecretpassword@localhost:5432/blacklist_db'
+    DB_HOST = os.environ.get('DB_HOST')
+    DB_PORT = os.environ.get('DB_PORT') or '5432'
+    DB_NAME = os.environ.get('DB_NAME')
+    DB_USER = os.environ.get('DB_USER')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+    if DB_HOST and DB_NAME and DB_USER and DB_PASSWORD:
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get(
+            'DATABASE_URL') or 'postgresql://postgres:mysecretpassword@localhost:5432/blacklist_db'
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'super-secret'
